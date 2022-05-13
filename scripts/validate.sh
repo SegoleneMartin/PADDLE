ad743f8082405bfc08ecb3920312097234e3ddb4
Segolene <sego91140@hotmail.fr>
2022-05-13 12:16:20 +0200

    validate.sh mis a jour

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -24,10 +24,18 @@ mkdir -p ${DATASET_DIR}
 
 # Defining all posssibilites
 
-DATASETS=("mini_imagenet" "tiered_imagenet" "meta_inat")
-METHODS=("ici" "pt_map" "tim_gd")
-PARAMETERS_TO_TUNE=("d" "")
-VALUES=("0.1 0.5 1.0" )
+DATASETS=("mini_imagenet" "tiered_imagenet" "inatural")
+METHODS=("alpha_tim" "tim" "laplacianshot" "baseline" "pt_map" "km_unbiased" "ici" "bdcspn")
+PARAMETERS_TO_TUNE=("alpha" "loss_weights" "lmd" "iter" "alpha" "alpha" "d" "temp")
+VALUES=("[4.0,4.0,4.0] [5.0,5.0,5.0] [6.0,6.0,6.0] [7.0,7.0,7.0] [8.0,8.0,8.0] [9.0,9.0,9.0] [10.0,10.0,10.0]" \
+"[1.0,0.1,1.0] [1.0,0.2,1.0] [1.0,0.3,1.0] [1.0,0.4,1.0] [1.0,0.5,1.0] [1.0,0.6,1.0] [1.0,0.7,1.0]" \
+"[0.4,0.7] [0.5,0.7] [0.6,0.7] [0.7,0.7] [0.8,0.7] [0.9,0.7] [1.0,0.7]" \
+"10 20 30 40 50 60 70" \
+"0.004 0.008 0.02 0.06 0.1 0.2 0.4 0.6" \
+"50 55 60 65 70 75 80" \
+"1 3 5 7 9 11 13" \
+"2 4 6 8 10 12" \
+)
 
 # Defining all required for the current simulation
 
@@ -42,6 +50,7 @@ tar xf ~/scratch/sego/data/${dataset}.tar.gz -C ${DATASET_DIR}
 for method in ${METHODS}; do \
     for value in ${parameter_values}; do \
         python3 -m main --base_config config/dirichlet/base_config/${architecture}/${dataset}/base_config.yaml \
-                        --method_config config/dirichlet/methods_config/${method}.yaml ;\
-                        opts ${parameter_name} ${value}
+                        --method_config config/dirichlet/methods_config/${method}.yaml \
+                        opts ${parameter_name} ${value} n_ways 5 num_classes_test 5 ;\
+    done
 done
